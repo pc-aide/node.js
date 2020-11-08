eae31e151c982744cc2bb5a090bc227d1a9593c4
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-11-07 20:49:47 -0500

    Delete getToppingName.js

--- pizza-luvrs-s3/templates/helpers/getToppingName.js
+++
@@ -1,8 +0,0 @@
-const { find } = require('lodash')
-
-module.exports = (toppingId, toppings) => {
-  const topping = find(toppings, (topping) => {
-    return topping.id === toppingId
-  })
-  return topping ? topping.name : toppingId
-}
