0336910b098ab81afbbd722edf7b1def0fab2fc6
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-11-07 20:51:59 -0500

    Delete pizza.js

--- pizza-luvrs-s3/models/pizza.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (name, toppings, img, username) {
-  this.id = name.replace(/ /g, '-')
-  this.name = name
-  this.toppings = toppings
-  this.img = img
-  this.username = username
-  this.created = (new Date()).getTime()
-}
