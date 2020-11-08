d6079d6400f8975001709800c4ded6c3de85eaea
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-11-07 20:51:52 -0500

    Delete topping.js

--- pizza-luvrs-s3/models/topping.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (id, name, previewImage, image, order) {
-  this.id = id
-  this.name = name
-  this.previewImage = previewImage
-  this.image = image
-  this.order = order
-}
